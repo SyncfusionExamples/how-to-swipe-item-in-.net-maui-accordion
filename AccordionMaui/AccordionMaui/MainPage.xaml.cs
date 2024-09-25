b94a105a7d5775cf2433b716ef35cce09ab5b151
JayashreeRavishankar <jayashree.sf3546@gmail.com>
2024-09-25 18:33:54 +0530

    Added sample

---
+++ AccordionMaui/AccordionMaui/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace AccordionMaui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }       
+    }
+
+}
---
+++ AccordionMaui/AccordionMaui/Model/ItemInfo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccordionMaui
+{
+    public class ItemInfo : INotifyPropertyChanged
+    {
+        #region Fields
+
+        private Color headerColor = Colors.Transparent;
+        #endregion
+
+        #region Properties
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+
+        public Color HeaderColor
+        {
+            get { return headerColor; }
+            set
+            {
+                headerColor = value;
+                this.OnPropertyChanged("HeaderColor");
+            }
+        }
+        #endregion
+
+        #region Interface
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged(string name)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+        #endregion
+    }
+}
